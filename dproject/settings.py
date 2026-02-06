ef8979ccd1e95a84b4d865c77d7f583608ebb250
Nazerke <241342@astanait.edu.kz>
2026-02-06 20:59:19 +0500

    Fix INSTALLED_APPS

--- dproject/settings.py
+++ dproject/settings.py
@@ -48,16 +48,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
  
     
+     'rest_framework',
+
+    # local apps
     'feedback',
-
-
-    
-
-
-    'rest_framework',
     'api',
-    'dproject',
-
+    
 ]
 
 MIDDLEWARE = [
